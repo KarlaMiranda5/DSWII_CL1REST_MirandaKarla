614253903b0d036a01014a17f7c754bf1b248897
KarlaMiranda5 <i201720871@cibertec.edu.pe>
2023-09-30 16:20:43 -0500

    1er Commit

---
+++ src/main/java/edu/pe/cibertec/apirestcibertec/model/MensajeResponse.java
@@ -0,0 +1,14 @@
+package edu.pe.cibertec.apirestcibertec.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.swing.*;
+
+@Data
+@Builder
+public class MensajeResponse {
+
+    private Spring resultado;
+
+}
