5b8ec7e09b98c972c648a515fed2e7a85f6012b1
KarlaMiranda5 <i201720871@cibertec.edu.pe>
2023-09-30 15:38:10 -0500

    Initial commit

---
+++ src/main/java/edu/pe/cibertec/apirestcibertec/ApiRestCibertecApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.apirestcibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCibertecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCibertecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/cibertec/apirestcibertec/ApiRestCibertecApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.apirestcibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCibertecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
